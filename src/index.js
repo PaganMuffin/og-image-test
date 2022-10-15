83aaad8788855ae102839254709bd88d95eab687
PaganMuffin <50595976+PaganMuffin@users.noreply.github.com>
2022-10-15 19:36:28 +0200

    renaming variables

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import { initWasm, Resvg } from "@resvg/resvg-wasm";
 import satori, { init } from "satori/wasm";
 import initYoga from "yoga-wasm-web";
-import wasmModule from "../node_modules/yoga-wasm-web/dist/yoga.wasm";
-import wasmModule2 from "../node_modules/@resvg/resvg-wasm/index_bg.wasm";
+import yogaWasm from "../node_modules/yoga-wasm-web/dist/yoga.wasm";
+import resvgWasm from "../node_modules/@resvg/resvg-wasm/index_bg.wasm";
 
 export default {
     async fetch(request, env, ctx) {
         if (request.url.includes("favicon")) return new Response("");
 
-        const title = new URL(request.url).searchParams.get("title") || "test";
-        const bg = new URL(request.url).searchParams.get("bg") || "white";
+        const urlSearch = new URL(request.url).searchParams;
+
+        const title = urlSearch.get("title") || "test";
+        const bg = urlSearch.get("bg") || "white";
+        const width = Number(urlSearch.get("w")) || 200;
+        const height = Number(urlSearch.get("h")) || 200;
         const f = await fetch(
             "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.1/fonts/roboto/Roboto-Regular.ttf"
         );
         const robotoArrayBuffer = await f.arrayBuffer();
-        const yoga = await initYoga(wasmModule);
+        const yoga = await initYoga(yogaWasm);
         init(yoga);
 
         const svg = await satori(
@@ -24,16 +28,15 @@ export default {
                     children: title,
                     style: {
                         backgroundColor: bg,
-                        height: 200,
-                        width: 200,
+                        height: height,
+                        width: width,
                         wordBreak: "break-all",
-                        textAlign: "center",
                     },
                 },
             },
             {
-                width: 200,
-                height: 200,
+                height: height,
+                width: width,
                 fonts: [
                     {
                         name: "Roboto",
@@ -45,7 +48,7 @@ export default {
             }
         );
         try {
-            await initWasm(wasmModule2);
+            await initWasm(resvgWasm);
         } catch (e) {}
         const resvg = new Resvg(svg);
         const pngData = resvg.render();
